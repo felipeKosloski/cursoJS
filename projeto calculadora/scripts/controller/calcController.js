dfcc7cdbcc21f1b28fc1e862f0c9315704ba54cd
Felipe Kosloski <felipe.kosloski.tkd@gmail.com>
2020-07-01 11:24:53 -0400

    basic files

---
+++ projeto calculadora/scripts/controller/calcController.js
@@ -0,0 +1,34 @@
+class CalcController {
+    
+    constructor(){
+        this._displayCalc = "0";
+        this._currentDate;
+        this.initialize;
+    }
+
+    initialize(){
+
+        
+
+    }
+
+    get displayCalc(){
+
+        return this._displayCalc;
+    }
+    set displayCalc(value){
+
+        this._displayCalc = value;
+    }
+   
+    get currentDate(){
+
+        return this._displayCalc;
+    }
+    set dataAtual(value){
+
+        this._dataAtual = value;
+    }
+    
+
+}
