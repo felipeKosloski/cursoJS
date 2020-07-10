ce7dff2096bdea88c1cf5a7215d7d10f27969be8
felipeKosloski <felipe.kosloski.tkd@gmail.com>
2020-07-09 22:10:17 -0400

    mvc

---
+++ curso-javascript-projeto-usuarios-master/models/User.js
@@ -0,0 +1,17 @@
+class User {
+
+    constructor(name, gender, birt, country, email, password, photo, admin) {
+
+        this.name = name;
+        this.gender = gender;
+        this.birt = birt;
+        this.country = country;
+        this.email = email;
+        this.password = password;
+        this.photo = photo;
+        this.admin = admin;
+
+
+    }
+
+}
