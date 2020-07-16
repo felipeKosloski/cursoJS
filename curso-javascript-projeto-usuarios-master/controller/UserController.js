2a9862389c94a35dafd633ffbc1df4a38df1b73e
felipeKosloski <felipe.kosloski.tkd@gmail.com>
2020-07-16 07:22:43 -0400

    validação

--- curso-javascript-projeto-usuarios-master/controller/UserController.js
+++ curso-javascript-projeto-usuarios-master/controller/UserController.js
@@ -6,6 +6,7 @@ class UserController {
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit();
+        this.onEdit();
     }
 
     onSubmit() {
@@ -20,6 +21,8 @@ class UserController {
             btn.disabled = true;
 
             let values = this.getValues();
+
+            if (!values) return false;
 
             this.getPhoto().then(
                  (content) => {
@@ -38,6 +41,16 @@ class UserController {
                 }
             );
             
+        });
+
+    }
+
+    onEdit() {
+
+        document.querySelector('#box-user-update .btn-cancel').addEventListener('click', e=>{
+
+            this.showPanelCreate();
+
         });
 
     }
@@ -90,8 +103,16 @@ class UserController {
     getValues(){
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach((field, index)=>{
+
+            if(['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value){
+                
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+
+            }
 
             if (field.name == 'gender' && field.checked) {
                 
@@ -108,6 +129,10 @@ class UserController {
             }
         
         });
+
+        if(!isValid) {
+            return false;
+        }
     
         return new User(
             user.name, 
@@ -127,6 +152,8 @@ class UserController {
 
         let tr = document.createElement('tr');
 
+        tr.dataset.user = JSON.stringify(dataUser);
+
         tr.innerHTML = `
             <tr>
                 <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
@@ -135,13 +162,55 @@ class UserController {
                 <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
                 <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-primary btn-xs btn-edit btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
             </tr>
         `;
 
+        tr.querySelector('.btn-edit').addEventListener('click', e=>{
+
+            console.log(JSON.parse(tr.dataset.user));
+          
+            this.showPanelUpdate();
+
+        });
+
+
         this.tableEl.appendChild(tr); 
+
+        this.updateCount();
     }
 
+    showPanelCreate() {
+
+        document.querySelector("#box-user-create").style.display = 'block';
+        document.querySelector("#box-user-update").style.display = 'none';
+
+
+    }
+
+    showPanelUpdate() {
+
+        document.querySelector("#box-user-create").style.display = 'none';
+        document.querySelector("#box-user-update").style.display = 'block';
+
+    }
+
+    updateCount() {
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+
+        [...this.tableEl.children].forEach(tr=>{
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+            if (user._admin) numberAdmin++;
+        });
+
+        document.querySelector('#number-users').innerHTML = numberUsers;
+        document.querySelector('#number-admins').innerHTML = numberAdmin;
+    }
 }
